326bf89a50a97b2e1d5e7313da99df1dae7381f6
Dylan Taylor <dylanjt3@gmail.com>
2012-08-03 20:24:24 -0400

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Higherthought::Application.config.secret_token = '572fd8033507ae44b74ed9656c4171fedd04c3f74b0d5122d5e4709bf3a2b4ea9e6e0995f30a9f850d4b2298182ebfe6ef473e7c54dfcbb8fb03788e4f868277'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Higherthought::Application.config.session_store :cookie_store, key: '_higherthought_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Higherthought::Application.config.session_store :active_record_store
