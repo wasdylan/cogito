24f92d8e01bf065f356de5b109c001a09f6d3ac4
Dylan Taylor <dylanjt3@gmail.com>
2012-08-03 22:05:42 -0400

    user profile with activity stream and articles

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,14 @@ Higherthought::Application.routes.draw do
 
   resources :posts
 
+  match 'user/:id' => 'users#profile'
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
   resources :users, :only => [:show, :index]
+
+
 end
